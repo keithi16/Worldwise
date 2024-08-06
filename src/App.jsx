fa82a60ccc25a1bad9d131a598931baa8855d6dc
Luiz Fernando Keithi Shimogava <keithishimogava16@gmail.com>
2024-08-06 01:26:10 -0300

    Primeiro Commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Product from './pages/Product'
+import Pricing from './pages/Pricing'
+import HomePage from './pages/HomePage'
+import PageNotFound from "./pages/PageNotFound"
+import AppLayout from "./pages/AppLayout"
+import Login from "./pages/Login"
+
+const App = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="/login" element={<Login  />} />
+          
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<p>LIST</p>} />
+
+            <Route path="cities" element={<p>List of cities</p>} />
+            <Route path="countries" element={<p>List of countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
+
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
